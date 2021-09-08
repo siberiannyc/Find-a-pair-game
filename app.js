44ca16887ba86829ce02f6f833023a85718bb776
siberiannyc <87045952+siberiannyc@users.noreply.github.com>
2021-09-07 21:45:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+
 // general setup
 let container = document.querySelector('.container')
 let select = document.getElementById('pairs');
-
+let moveNumber = document.getElementById('movenumber')
+let reset = document.querySelector('.reset')
+let resultsModul = document.querySelector('.modul')
+let closeModul = document.querySelector('.closemodul')
+let restartBttn = document.querySelector('.restart')
+let scoreDigits = document.querySelector('.score-digits')
+let countDigits = document.querySelector('.count-digits')
+let iconTurn = document.querySelector('.icon-slide')
+let textSlide = document.querySelector('.text-slide')
 
 
 //Dynamic Tyles and Shuffle
 let dynamTiles = ()=> {
+  moveNumber.textContent = 0;
+  reset.classList.remove('resetfin')
+  let finalCount =0;
+  let score=0;
   let value = select.options[select.selectedIndex];
   let qty = value.getAttribute("value")
   let n = (Number(qty)) ;
   let tileCount= [];
+  let testTest = [];
+  console.log(testTest)
   tileCount =  Array.from(Array(n).keys());
   tileCount = tileCount.reduce(function (res, current) {
     return res.concat([current, current]);}, []);
@@ -39,6 +54,10 @@ let dynamTiles = ()=> {
    }).join('');
   container.innerHTML = tiles;
 
+
+
+
+
 //   Tile dimensions
  let tileStyle = document.querySelector('.tile');
  let tileWidth = tileStyle.getBoundingClientRect().width;
@@ -62,12 +81,46 @@ let dynamTiles = ()=> {
   let resultArr =[];
   let click = 0;
   let openedCards = [];
+  let matchedCards=[];
+ 
+ 
+
+  
 
   tileStyles.forEach(function(tile){
+      
     tile.addEventListener("click",
       function(e){
+          if(click < 2){
+              if( e.currentTarget.classList.contains('flipped-card')){}else{
       e.currentTarget.classList.add('flipped-card');
       click++;
+      score++;
+      finalCount = score/2;
+      let finalScore = 100;
+      let m = finalCount-(n+(n*0.5));
+
+      if(finalCount <= n+2){
+       finalScore = finalScore;
+       console.log(finalScore)
+      }else{
+          for(i=1;i<m; i++){
+              console.log(i)
+              finalScore = finalScore-(finalScore*0.1);
+              finalScore = Math.round(finalScore)
+          }
+          console.log(finalScore)
+
+      
+      }
+
+      if(Number.isInteger(finalCount)){
+      moveNumber.textContent = finalCount;
+      countDigits.textContent = finalCount;
+      scoreDigits.textContent = finalScore;
+    
+    };
+     
 
       let id = e.currentTarget.dataset.id;  
       openedCards.push(this);
@@ -82,18 +135,61 @@ let dynamTiles = ()=> {
       openedCards[1].classList.remove('flipped-card');
       openedCards =[];
       click = 0;
-   }, 1000);
+   }, 800);
   }else if(click === 2 && resultArr[0] === resultArr[1]){
     resultArr=[];
     setTimeout(function(){
         openedCards[0].children[0].children[1].classList.add('match');
         openedCards[1].children[0].children[1].classList.add('match');
+        matchedCards.push(openedCards[0]);
+        matchedCards.push(openedCards[1]);
+        console.log(matchedCards)
+        if(matchedCards.length === n*2){
+            resultsModul.classList.add('modul-show');
+        }
         openedCards =[];
         click = 0;
      }, 500);
    }
+}
+}else{};
+
+
+
   });
  });  
 }
 
 select.addEventListener('change',dynamTiles);
+reset.addEventListener('mousedown',function(){
+    iconTurn.classList.add('reset-click');
+    textSlide.classList.add('text-none')
+    dynamTiles();
+ 
+});
+
+closeModul.addEventListener('click', function(){
+    resultsModul.classList.remove('modul-show');
+    reset.classList.add('resetfin')
+});
+restartBttn.addEventListener('click', function(){
+    resultsModul.classList.remove('modul-show');
+    dynamTiles();
+    
+});
+
+const URL = 'https://picsum.photos/v2/list?page=2&limit=100';
+window.addEventListener('DOMContentLoaded', async ()=>{
+  let data = await fetch(URL);
+  let response = await data.json();
+  dataSet(response);
+}
+);
+
+let dataSet = (item)=>{
+  for(i=0;i<item.length;i++){
+    console.log(item[i].download_url)
+  }
+  };
+
+
