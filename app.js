40135ac1aea913c035a729b59da886cb721b0ba5
siberiannyc <87045952+siberiannyc@users.noreply.github.com>
2021-09-12 13:14:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,221 +1,260 @@
 // DOM Selectors
-  let container = document.querySelector('.container');
-  let select = document.getElementById('pairs');
-  let reset = document.querySelector('.reset');
-  let resultsModul = document.querySelector('.modul');
-  let closeModul = document.querySelector('.closemodul');
-  let restartBttn = document.querySelector('.restart');
-  let scoreDigits = document.querySelector('.score-digits');
-  let countDigits = document.querySelector('.count-digits');
-  let iconTurn = document.querySelector('.icon-slide');
-  let textSlide = document.querySelector('.text-slide');
-  let testTime = document.querySelector('.test');
-  var appendTens = document.getElementById("tens");
-  var appendSeconds = document.getElementById("seconds");
+let container = document.querySelector('.container');
+let select = document.getElementById('pairs');
+let reset = document.querySelector('.reset');
+let resultsModul = document.querySelector('.modul');
+let closeModul = document.querySelector('.closemodul');
+let restartBttn = document.querySelector('.restart');
+let scoreDigits = document.querySelector('.score-digits');
+let countDigits = document.querySelector('.count-digits');
+let iconTurn = document.querySelector('.icon-slide');
+let textSlide = document.querySelector('.text-slide');
+let testTime = document.querySelector('.test');
+var appendTens = document.getElementById("tens");
+var appendSeconds = document.getElementById("seconds");
 
 // Default Variables
-  var seconds = 00; 
-  var tens = 00; 
-  var Interval ;
-  let testTest =[];
-  let resultArr =[];
-  let click = 0;
-  let openedCards = [];
-  let matchedCards=[];
+var seconds = 00; 
+var tens = 00; 
+var Interval ;
+let testTest =[];
+let result = [];
+let resultArr =[];
+let click = 0;
+let openedCards = [];
+let matchedCards=[];
 
 // Functions
- // Start Timer
-  function startTimer () {
-    tens++; 
+// Start Timer
+function startTimer () {
+  tens++; 
 
-    if(tens <= 9){
-      appendTens.innerHTML = "0" + tens;
-    }
+  if(tens <= 9){
+    appendTens.innerHTML = "0" + tens;
+  }
+  
+  if (tens > 9){
+    appendTens.innerHTML = tens;
     
-    if (tens > 9){
-      appendTens.innerHTML = tens;
-      
-    } 
-    
-    if (tens > 99) {
-      console.log("seconds");
-      seconds++;
-      appendSeconds.innerHTML = "0" + seconds;
-      tens = 0;
-      appendTens.innerHTML = "0" + 0;
-    }
-    
-    if (seconds > 9){
-      appendSeconds.innerHTML = seconds;
-    }
+  } 
+  
+  if (tens > 99) {
+    // console.log("seconds");
+    seconds++;
+    appendSeconds.innerHTML = "0" + seconds;
+    tens = 0;
+    appendTens.innerHTML = "0" + 0;
+  }
+  
+  if (seconds > 9){
+    appendSeconds.innerHTML = seconds;
+  }
 
-  };
+};
 
- // Reset Timer
-  let timerReset = function() {
-    clearInterval(Interval);
-  tens = "00";
-  seconds = "00";
-  appendTens.innerHTML = tens;
-  appendSeconds.innerHTML = seconds;
-  };
+// Reset Timer
+let timerReset = function() {
+  clearInterval(Interval);
+tens = "00";
+seconds = "00";
+appendTens.innerHTML = tens;
+appendSeconds.innerHTML = seconds;
+};
 
 // Fetch pictures from server
-  const URL = 'https://picsum.photos/v2/list?page=2&limit=100';
-  window.addEventListener('DOMContentLoaded', extData()
-  );
+const URL = 'https://emoji-api.com/emojis?access_key=bbc936f99d2b4d38f46163d09520f7135d241812';
+window.addEventListener('DOMContentLoaded', extData()
+);
 
-  async function extData(){
-    let data = await fetch(URL);
-    let response = await data.json();
-    let linkReseived = response.map(function(item){
-      testTest.push(item.download_url);
-    });
-  }
+async function extData(){
+  let data = await fetch(URL);
+  let response = await data.json();
+  let linkReseived = response.map(function(item){
+    testTest.push(item);
+    
+  });
+}
 
 
 //Dynamic Tyles
-  let dynamTiles = ()=> {
-    // Reset to default
-      timerReset();
-      reset.classList.remove('resetfin');
-      let finalCount =0;
-      let score=0;
-    // Catch pair qty
-      let value = select.options[select.selectedIndex];
-      let qty = value.getAttribute("value");
-      let n = (Number(qty));
-    // Refresh Pictures Array
-      extData();
-      const shuffleArray = array => {
-        for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        };
-      }
-      shuffleArray(testTest);
+let dynamTiles = ()=> {
+  // Reset to default
+    timerReset();
+    reset.classList.add('reset-disable');
+    textSlide.classList.add('text-slide-dis');
+    reset.classList.remove('resetfin');
+    let finalCount =0;
+    let score=0;
+    let matchedCards=[];
+  // Catch pair qty
+    let value = select.options[select.selectedIndex];
+    let qty = value.getAttribute("value");
+    let n = (Number(qty));
+  // Refresh Pictures Array
+    extData();
+     result = testTest.filter(rawRes => rawRes.codePoint.length === 5);
+    const shuffleArray = array => {
+      for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+      };
+    }
+    shuffleArray(result);
 
-      testTest = testTest.slice(0,n);
-      testTest = testTest.reduce(function (res, current) {
-        return res.concat([current, current])}, []);
-      shuffleArray(testTest);
 
-    //HTML Dynamic Code
-      let tiles = testTest.map(function(item){
-      return `<div class="flip-card tile" data-id="${item.slice(25,28)}">
-                <div class="flip-card-inner">
-                <div class="flip-card-front" >
-                <i class="fas fa-question"></i>
+console.log(result);
+
+    result = result.slice(0,n);
+    result = result.reduce(function (res, current) {
+      return res.concat([current, current])}, []);
+    shuffleArray(result);
+    console.log(result);
+  //HTML Dynamic Code
+    let tiles = result.map(function(item){
+    return `<div class="flip-card tile" data-id="${item.codePoint}">
+              <div class="flip-card-inner">
+              <div class="flip-card-front" >
+              <i class="fas fa-question"></i>
+            </div>
+            <div class="flip-card-back" >
+              <span>&#x${item.codePoint}</span>
+            </div>
               </div>
-              <div class="flip-card-back" >
-                <img src="${item}" alt="random picture">
-              </div>
-                </div>
-              </div>`;    
-      }).join('');
-      console.log(tiles);
-      container.innerHTML = tiles;
+            </div>`;    
+    }).join('');
+    console.log(tiles);
+    container.innerHTML = tiles;
 
-    //Tile dimensions
-      let tileStyle = document.querySelector('.tile');
-      let tileWidth = tileStyle.getBoundingClientRect().width;
-      let tileHeight = tileStyle.getBoundingClientRect().height;
-      if(qty === "4"){
-        container.style.width = ((tileWidth+1)*testTest.length)/2 +'px';
-        container.style.height = ((tileHeight*2)+2) + 'px';
-        }else if(qty === "18"){
-        container.style.width = ((tileWidth+1)*testTest.length)/6 +'px';
-        container.style.height = ((tileHeight*6)+2) + 'px';}
-        else if(qty === "20"){
-            container.style.width = ((tileWidth+1)*testTest.length)/5 +'px';
-            container.style.height = ((tileHeight*5)+2) + 'px';}
-        else{
-        container.style.width = ((tileWidth+1)*testTest.length)/4 +'px';
-        container.style.height = ((tileHeight*4)+2) + 'px';}
+  //Tile dimensions
+    let tileStyles = document.querySelectorAll('.tile');  
 
-      let tileStyles = document.querySelectorAll('.tile');   
+    if(qty === "4"){
+      tileStyles.forEach(function(tile){
+        tile.classList.add('tile_4')
+      })
+      }else if(qty === "6"){
+        tileStyles.forEach(function(tile){
+          tile.classList.add('tile_6')
+        })}else if(qty === "8"){
+          tileStyles.forEach(function(tile){
+            tile.classList.add('tile_8')
+          })}else if(qty === "10"){
+            tileStyles.forEach(function(tile){
+              tile.classList.add('tile_10')
+            })}else if(qty === "12"){
+              tileStyles.forEach(function(tile){
+                tile.classList.add('tile_12')
+              })}else if(qty === "14"){
+                tileStyles.forEach(function(tile){
+                  tile.classList.add('tile_14')
+                })}else if(qty === "16"){
+                  tileStyles.forEach(function(tile){
+                    tile.classList.add('tile_16')
+                  })}else if(qty === "18"){
+                    tileStyles.forEach(function(tile){
+                      tile.classList.add('tile_18')
+                    })}else if(qty === "20"){
+                      tileStyles.forEach(function(tile){
+                        tile.classList.add('tile_20')
+                      })}
+ 
+
+
+   
 
 // Game Mechanics
-  tileStyles.forEach(function(tile){   
-    tile.addEventListener("click",
-      function(e){
-        // start timer
-          if(click === 0){
-          clearInterval(Interval);
-          Interval = setInterval(startTimer, 10);
+tileStyles.forEach(function(tile){   
+  tile.addEventListener("click",
+    function(e){
+      // start timer
+        if(click === 0){
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+        };
+        //add Restart button;
+        if(click === 0){
+          reset.classList.remove('reset-disable');
+          textSlide.classList.remove('text-slide-dis');
+        }
+      // Before 2 cards flipped
+        if(click < 2){
+        if( e.currentTarget.classList.contains('flipped-card')){}else{
+          e.currentTarget.classList.add('flipped-card');
+          click++;
+          score++;
+          // Moves Count
+            finalCount = score/2;
+            if(Number.isInteger(finalCount)){
+            countDigits.textContent = finalCount;
           };
-        // Before 2 cards flipped
-          if(click < 2){
-          if( e.currentTarget.classList.contains('flipped-card')){}else{
-            e.currentTarget.classList.add('flipped-card');
-            click++;
-            score++;
-            // Moves Count
-              finalCount = score/2;
-              if(Number.isInteger(finalCount)){
-              countDigits.textContent = finalCount;
-            };
-            // Cards Comparison
-              let id = e.currentTarget.dataset.id;  
-              openedCards.push(this);
-              resultArr.push(id);
-            //  Not matched
-              if(click === 2 && resultArr[0] !== resultArr[1]){     
+          // Cards Comparison
+            let id = e.currentTarget.dataset.id;  
+            openedCards.push(this);
+            resultArr.push(id);
+          //  Not matched
+            if(click === 2 && resultArr[0] !== resultArr[1]){     
+            resultArr=[];
+            // console.log(resultArr)
+            setTimeout(function(){
+              openedCards[0].classList.remove('flipped-card');
+              openedCards[1].classList.remove('flipped-card');
+              openedCards =[];
+              click = 0;
+            }, 800);
+          //  matched
+            }else if(click === 2 && resultArr[0] === resultArr[1]){
               resultArr=[];
-              console.log(resultArr)
               setTimeout(function(){
-                openedCards[0].classList.remove('flipped-card');
-                openedCards[1].classList.remove('flipped-card');
+                openedCards[0].children[0].children[1].classList.add('match');
+                openedCards[1].children[0].children[1].classList.add('match');
+                matchedCards.push(openedCards[0]);
+                matchedCards.push(openedCards[1]);
+                // console.log(matchedCards);
+                // Game Finished
+                if(matchedCards.length === n*2){
+                  resultsModul.classList.add('modul-show');
+                  clearInterval(Interval);
+                  scoreDigits.textContent = seconds + ':' + tens;
+                  container.innerHTML = `<p>Choose a game size or press START OVER button</p>
+                  <span>
+                  <img src="./Media/dm4uz3-foekoe.gif" alt="zzz" id="zzz">
+              </span>`
+                }
                 openedCards =[];
                 click = 0;
-              }, 800);
-            //  matched
-              }else if(click === 2 && resultArr[0] === resultArr[1]){
-                resultArr=[];
-                setTimeout(function(){
-                  openedCards[0].children[0].children[1].classList.add('match');
-                  openedCards[1].children[0].children[1].classList.add('match');
-                  matchedCards.push(openedCards[0]);
-                  matchedCards.push(openedCards[1]);
-                  console.log(matchedCards);
-                  // Game Finished
-                  if(matchedCards.length === n*2){
-                    resultsModul.classList.add('modul-show');
-                    clearInterval(Interval);
-                    scoreDigits.textContent = seconds + ':' + tens;
-                  }
-                  openedCards =[];
-                  click = 0;
-                }, 500);
-              }
-          }
-          }else{};
-      });
-  });  
-  }
+              }, 500);
+            }
+        }
+        }else{};
+    });
+});  
+}
 
- // Event Listeners
-  // Tiles Refresh When Qty Selected
-    select.addEventListener('change',dynamTiles);
-  // Start Over Button Pressed
-    reset.addEventListener('mousedown',function(){
-      iconTurn.classList.add('reset-click');
-      textSlide.classList.add('text-none');
-      dynamTiles();
-      timerReset();
-    });
-  // Results Chart Closed with X
-    closeModul.addEventListener('click', function(){
-      resultsModul.classList.remove('modul-show');
-      reset.classList.add('resetfin');
-    });
-  // Play Again button in the chart pressed
-restartBttn.addEventListener('click', function(){
+// Event Listeners
+// Tiles Refresh When Qty Selected
+  select.addEventListener('change',dynamTiles);
+// Start Over Button Pressed
+  reset.addEventListener('mousedown',function(){
+
+    dynamTiles();
+    timerReset();
+  });
+// Results Chart Closed with X
+  closeModul.addEventListener('click', function(){
     resultsModul.classList.remove('modul-show');
-      dynamTiles();
-      timerReset();
-    }); 
+    reset.classList.add('resetfin');
+  });
+// Play Again button in the chart pressed
+restartBttn.addEventListener('click', function(){
+  resultsModul.classList.remove('modul-show');
+    dynamTiles();
+    timerReset();
+  }); 
+  window.addEventListener("DOMContentLoaded",function(){
+    reset.classList.add('reset-disable');
+    textSlide.classList.add('text-slide-dis');
+  })
 
 // Code Finished
